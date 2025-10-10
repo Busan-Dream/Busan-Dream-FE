f85e2aec4117cbe56bf8335f1beaf21f5737b1df
miseullang <miseullang@gmail.com>
2025-10-10 21:57:46 +0900

    💄 Style: 404 페이지 스타일 수정

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -3,24 +3,20 @@ import BlurText from "@/components/ReactBits/BlurText/BlurText";
 const Error = () => {
   return (
     <div className="min-h-screen w-full flex flex-col items-center justify-center bg-gray-50">
-      <div className="mb-8">
-        <BlurText
-          text="404"
-          delay={100}
-          animateBy="letters"
-          direction="top"
-          className="text-[8rem] font-black text-gray-800 font-paperozi"
-        />
-      </div>
-      <div>
-        <BlurText
-          text="페이지를 찾을 수 없습니다."
-          delay={150}
-          animateBy="words"
-          direction="top"
-          className="text-6xl font-black text-gray-800 font-paperozi"
-        />
-      </div>
+      <BlurText
+        text="404"
+        delay={100}
+        animateBy="letters"
+        direction="top"
+        className="text-[8rem] font-black text-gray-800 font-paperozi"
+      />
+      <BlurText
+        text="페이지를 찾을 수 없습니다."
+        delay={150}
+        animateBy="words"
+        direction="top"
+        className="text-4xl font-black text-gray-800 font-paperozi"
+      />
     </div>
   );
 };
