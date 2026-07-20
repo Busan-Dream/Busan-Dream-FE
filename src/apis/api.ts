58454de0a0965451f67e083a66d07f329eb63520
miseullang <miseullang@gmail.com>
2026-07-20 09:24:17 +0900

    📝 Chore: 타임아웃 관련 주석 추가

--- src/apis/api.ts
+++ src/apis/api.ts
@@ -4,7 +4,7 @@ const API_BASE_URL = "https://busan-dream.co.kr";
 
 export const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
-  timeout: 600000,
+  timeout: 600000, // 백엔드 -> LLM 분석 -> 프론트엔드까지 최대 10분 소요될 수 있으므로 타임아웃을 10분으로 설정
   headers: {
     "Content-Type": "application/json",
   },
